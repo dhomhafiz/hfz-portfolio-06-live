efbb935584c762a831c828e92db1d8d0088cfdd5
dhomhafiz@gmail.com <dhomhafiz@gmail.com>
2026-02-18 17:00:47 +0800

    Update project section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -10,7 +10,7 @@ function Project() {
             <div className="project">
                 <a href="https://www.therakyatpost.com/fun/2024/12/20/come-and-play-ddakji-squid-game-takes-over-mrt-ampang-park/" target="_blank" rel="noreferrer"><img src={mock1} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://www.therakyatpost.com/fun/2024/12/20/come-and-play-ddakji-squid-game-takes-over-mrt-ampang-park/" target="_blank" rel="noreferrer"><h2>Netflix's Squid Game Poll System</h2></a>
-                <p>Built the Netflix Squid Game interactive poll system as featured in the series using Python.</p>
+                <p>Built the Netflix Squid Game interactive poll system as featured in the series using Python as part of Netflix's promotion of the series at MRT Station Ampang Park, KL, Malaysia.</p>
                 <p>The poll system is triggered when a player chooses to continue the game or withdraw. “X” indicates that the player chose not to continue, while “O” indicates continuation. When a player clicks the colored button, blue represents “O” (continue) and red represents “X” (withdraw).</p>
             </div>
         </div>
